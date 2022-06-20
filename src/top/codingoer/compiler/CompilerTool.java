43bb3b07934e171cf384f80ba4ccf91b0cc9a7f1
codingoer <codingoer@163.com>
2022-06-20 23:47:09 +0800

    add JavaCompiler test

---
+++ src/top/codingoer/compiler/CompilerTool.java
@@ -0,0 +1,20 @@
+package top.codingoer.compiler;
+
+import javax.lang.model.SourceVersion;
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+
+public class CompilerTool {
+
+    public static void main(String[] args) {
+        compilerWithOptions();
+    }
+
+    public static void compilerWithOptions() {
+        final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        for( final SourceVersion version: compiler.getSourceVersions() ) {
+            System.out.println( version );
+        }
+    }
+
+}
