44a5e717c650278f5c821d346e888a40c4104b2a
codingoer <codingoer@163.com>
2022-06-22 20:16:53 +0800

    compiler test

--- src/top/codingoer/compiler/CompilerTool.java
+++ src/top/codingoer/compiler/CompilerTool.java
@@ -2,19 +2,32 @@ package top.codingoer.compiler;
 
 import javax.lang.model.SourceVersion;
 import javax.tools.JavaCompiler;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.Tool;
 import javax.tools.ToolProvider;
+import java.util.ServiceLoader;
 
 public class CompilerTool {
 
     public static void main(String[] args) {
-        compilerWithOptions();
+        //compilerWithOptions();
+        serviceLoader();
     }
 
     public static void compilerWithOptions() {
-        final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        for( final SourceVersion version: compiler.getSourceVersions() ) {
+        final JavaCompiler javac = ToolProvider.getSystemJavaCompiler();
+        for( final SourceVersion version: javac.getSourceVersions() ) {
             System.out.println( version );
         }
+
+        //StandardJavaFileManager standardFileManager = javac.getStandardFileManager(null, null, null);
+        //standardFileManager.getJavaFileForInput();
     }
 
+    public static void serviceLoader() {
+        ServiceLoader<Tool> tools = ServiceLoader.loadInstalled(Tool.class);
+        for (Tool tool : tools) {
+            System.out.println("Found tool: " + tool);
+        }
+    }
 }
